4071f64e2a6208cd9945325b24a562b1b2999aea
Chinh Nguyen <ng.the.chinh@gmail.com>
2023-08-25 17:12:46 +0700

    Deploy to iOS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'ionic.photogallery',
   appName: 'photo-gallery',
   webDir: 'www',
   server: {
